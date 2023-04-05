a4632bef25bb3b7fd2aec88027b1a92b9557682b
xiexinyue <xxy295518311@qq.com>
2023-04-05 15:46:12 +0800

    更新README

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # bincular-vision
 双目视觉3D重建
+bincularTest.py 为双目相机测试程序。
+
+BM.py 为基于BM算法实现三维重建的算法，精度不高，但是速度较快，双目标定数据已使用matlab工具箱标定获得。
+
+distance.py为使用opencv库实现双目标定+基于BM算法实现三维重建的算法（为某git仓库的参考算法）。
+
+SGBM.py为基于SGBM算法实现三维重建的算法,效果尚可，但速度很慢，双目标定数据已使用matlab工具箱标定获得。
+
+SGBM_WLS.py为基于SGBM算法+WLS滤波算法实现三维重建的算法,效果尚可，速度较快，可以基于此文件做进一步优化。双目标定数据已使用matlab工具箱标定获得。
+
+caliParas.mat 为使用matlab工具箱获得的双目相机的标定参数。
+
+pycharmEvir.png为所使用的工具包版本信息。
+
--- SGBM_WLS.py
+++ SGBM_WLS.py
@@ -17,6 +17,7 @@ def preprocess(img1, img2):
 
     return img1, img2
 
+# 双目相机的标定参数
 left_camera_matrix_tmp = np.array([[748.537220645527, 0., 0.],
                                [-0.171086862715876, 748.573756045103, 0.],
                                [620.640398806220, 347.086603859931, 1.]])
@@ -38,7 +39,8 @@ R = np.array([[0.999991101510547, -0.000820710991406780, 0.00413803494327793],
 
 T = np.array([-59.4156595079536, -0.118520398706135, -1.39762490025346])  # 平移关系向量TranslationOfCamera2
 
-size = (1280, 720)  # 图像尺寸
+# 单目图像尺寸
+size = (1280, 720)
 
 # 进行立体更正
 R1, R2, P1, P2, Q, validPixROI1, validPixROI2 = cv2.stereoRectify(left_camera_matrix, left_distortion,
@@ -48,6 +50,7 @@ R1, R2, P1, P2, Q, validPixROI1, validPixROI2 = cv2.stereoRectify(left_camera_ma
 left_map1, left_map2 = cv2.initUndistortRectifyMap(left_camera_matrix, left_distortion, R1, P1, size, cv2.CV_16SC2)
 right_map1, right_map2 = cv2.initUndistortRectifyMap(right_camera_matrix, right_distortion, R2, P2, size, cv2.CV_16SC2)
 
+# 以下window为调试时使用
 cv2.namedWindow("left")
 cv2.namedWindow("right")
 cv2.namedWindow("depth")
@@ -61,16 +64,14 @@ camera.set(cv2.CAP_PROP_FRAME_WIDTH, 2560)  # 1920,1280
 camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)  # 1080,720
 
 
-# camera2 = cv2.VideoCapture(1)
-
-# 添加点击事件，打印当前点的距离
+# 添加点击事件，打印当前点的3D坐标
 def callbackFunc(e, x, y, f, p):
     if e == cv2.EVENT_LBUTTONDOWN:
         print(threeD[y][x])
 
 
 cv2.setMouseCallback("depth", callbackFunc, None)
-blockSize = 3
+
 while True:
     ret, frame = camera.read()
     if not ret:
@@ -82,32 +83,7 @@ while True:
     frame1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
     frame2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
 
-    # 重映射，就是把一幅图像中某位置的像素放置到另一个图片指定位置的过程。
-    # 依据MATLAB测量数据重建无畸变图片,输入图片要求为灰度图
-    # img1_rectified = cv2.remap(frame1, left_map1, left_map2, cv2.INTER_AREA)
-    # img2_rectified = cv2.remap(frame2, right_map1, right_map2, cv2.INTER_AREA)
-    #
-    # # 消除畸变
-    # frame1 = cv2.undistort(frame1, left_camera_matrix, left_distortion)
-    # frame2 = cv2.undistort(frame2, right_camera_matrix, right_distortion)
-    # # 立体匹配
-    # iml_, imr_ = preprocess(frame1, frame2)  # 预处理，一般可以削弱光照不均的影响，不做也可以
-
-    # 转换为opencv的BGR格式
-    # imageL = cv2.cvtColor(img1_rectified, cv2.COLOR_GRAY2BGR)
-    # imageR = cv2.cvtColor(img2_rectified, cv2.COLOR_GRAY2BGR)
-
-    # ------------------------------------SGBM算法----------------------------------------------------------
-    #   blockSize                   深度图成块，blocksize越低，其深度图就越零碎，0<blockSize<10
-    #   img_channels                BGR图像的颜色通道，img_channels=3，不可更改
-    #   numDisparities              SGBM感知的范围，越大生成的精度越好，速度越慢，需要被16整除，如numDisparities
-    #                               取16、32、48、64等
-    #   mode                        sgbm算法选择模式，以速度由快到慢为：STEREO_SGBM_MODE_SGBM_3WAY、
-    #                               STEREO_SGBM_MODE_HH4、STEREO_SGBM_MODE_SGBM、STEREO_SGBM_MODE_HH。精度反之
-    # ------------------------------------------------------------------------------------------------------
-    # blockSize = 3
-
-    # 两个trackbar用来调节不同的参数查看效果
+    # 三个trackbar用来调节不同的参数查看效果，确定参数后就不需要了
     num = cv2.getTrackbarPos("num", "depth")
     blockSize = cv2.getTrackbarPos("blockSize", "depth")
     minDisparity = cv2.getTrackbarPos("minDisparity", "depth")
@@ -117,13 +93,20 @@ while True:
 
     blockSize = 3
     # num = 13
-    num = 4
+    num = 7
     img_channels = 3
     minDisparity = 0
-
+    # ------------------------------------SGBM算法----------------------------------------------------------
+    #   blockSize                   深度图成块，blocksize越低，其深度图就越零碎，0<blockSize<10
+    #   img_channels                BGR图像的颜色通道，img_channels=3，不可更改
+    #   numDisparities              SGBM感知的范围，越大生成的精度越好，速度越慢，需要被16整除，如numDisparities
+    #                               取16、32、48、64等
+    #   mode                        sgbm算法选择模式，以速度由快到慢为：STEREO_SGBM_MODE_SGBM_3WAY、
+    #                               STEREO_SGBM_MODE_HH4、STEREO_SGBM_MODE_SGBM、STEREO_SGBM_MODE_HH。精度反之
+    # ------------------------------------------------------------------------------------------------------
     left_matcher = cv2.StereoSGBM_create(
         minDisparity=0,
-        numDisparities=7*16,  # max_disp has to be dividable by 16 f. E. HH 192, 256
+        numDisparities=num*16,
         blockSize=3,
         disp12MaxDiff=1,
         uniquenessRatio=15,
@@ -148,15 +131,13 @@ while True:
     displ = np.int16(displ)
     dispr = np.int16(dispr)
 
-    filteredImg = wls_filter.filter(displ, frame1, None, dispr)  # important to put "imgL" here!!!
+    filteredImg = wls_filter.filter(displ, frame1, None, dispr)
 
     filteredImg = cv2.normalize(src=filteredImg, dst=filteredImg, beta=0, alpha=255, norm_type=cv2.NORM_MINMAX);
     filteredImg = np.uint8(filteredImg)
 
     # 计算三维坐标数据值
     threeD = cv2.reprojectImageTo3D(filteredImg, Q, handleMissingValues=True)
-    # # 计算出的threeD，需要乘以16，才等于现实中的距离
-    # threeD = threeD * 16
 
     # cv2.imshow("left", img1_rectified)
     # cv2.imshow("right", img2_rectified)
@@ -167,10 +148,9 @@ while True:
     if key == ord("q"):
         break
     # elif key == ord("s"):
-    #     cv2.imwrite("E:/pythonlearning/opencv/screenshot/BM_left.jpg", imgL)
-    #     cv2.imwrite("E:/pythonlearning/opencv/screenshot/BM_right.jpg", imgR)
-    #     cv2.imwrite("E:/pythonlearning/opencv/screenshot/BM_depth.jpg", disp)
+    #     cv2.imwrite("", imgL)
+    #     cv2.imwrite("", imgR)
+    #     cv2.imwrite("", disp)
 
 camera.release()
-# camera2.release()
 cv2.destroyAllWindows()
